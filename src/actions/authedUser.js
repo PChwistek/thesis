0fc1bc9f96895c04875d5b020082aa519ab9f408
Philip  Chwistek <chwistek.philip@gmail.com>
2018-09-24 17:00:26 -0400

    re-initializing the project

--- src/actions/authedUser.js
+++
@@ -1,9 +0,0 @@
-//the user, for now, is basically a wallet address
-export const SET_AUTHED_USER = 'SET_AUTHED_USER'
-
-export function setAuthedUser(address) {
-  return {
-    type: SET_AUTHED_USER,
-    address
-  }
-}
