63b1487876f49ed16b964d303470ab8c9f151b14
Philip Chwistek <mrsportsnerd@gmail.com>
2018-12-28 03:06:42 -0500

    merged into 1 contract

--- contracts/submerged/submerged.cpp
+++ contracts/submerged/submerged.cpp
@@ -1,12 +1,13 @@
-#include "submanager.hpp"
+#include "submerged.hpp"
 using namespace eosio;
 using std::string;
 
-ACTION submanager::version() {
+
+ACTION submerged::version() {
   print("Version 0.2");
 }
 
-ACTION submanager::subscribe() {
+ACTION submerged::transfer() {
   auto transfer_data = eosio::unpack_action_data<st_transfer>();
   name from = transfer_data.from;
   name to = name(transfer_data.memo);
@@ -20,7 +21,7 @@ ACTION submanager::subscribe() {
     eosio_assert(quantity.amount >= theChannel.minimum_price.amount, "insufficient funds");
     eosio_assert(quantity.symbol.code() == theChannel.minimum_price.symbol.code(), "incorrect symbol");
 
-    channelsubs_table subs(_self, to.value);
+    subs_table subs(_self, to.value);
     auto iterator = subs.find( from.value );
     if( iterator == subs.end() ) {
       subs.emplace(_self, [&]( auto& row){
@@ -35,41 +36,64 @@ ACTION submanager::subscribe() {
   }  else if( _self == from) {
     name content_creator = transfer_data.to;
     print("============ Removing ===============");
-    channelsubs_table subs(_self, content_creator.value);
+    subs_table subs(_self, content_creator.value);
     auto iterator = subs.find(to.value);
     eosio_assert(iterator != subs.end(), "Record does not exist");
     subs.erase(iterator);
   }
 }
 
-ACTION submanager::openchannel(name owner, asset minimum_price) {
+
+ACTION submerged::open(name owner, asset minimum_price) {
   require_auth( owner );
   channels_table channels(_self, _self.value);
   auto iterator = channels.find(owner.value);
   if( iterator == channels.end() ) {
     channels.emplace(owner, [&]( auto& row ) {
       row.key = owner;
+      row.sub_status = "pending";
       row.minimum_price = minimum_price;
     });
   } else {
     print("Store already exists.");
     channels.modify(iterator, owner, [&]( auto& row ) {
       row.key = owner;
+      row.sub_status = "pending";
       row.minimum_price = minimum_price;
     });
   }
 }
 
-ACTION submanager::rollfunds(name content_creator, name subber) {
+ACTION submerged::rollfunds(name content_creator, name subber) {
   require_auth( _self );
-  channelsubs_table subs(_self, content_creator.value);
+  subs_table subs(_self, content_creator.value);
   auto theSub = subs.get(subber.value);
   asset quantity = theSub.quantity_subscribed;
   send_money(content_creator, quantity, subber.to_string());
 }
 
+ACTION submerged::setproject(name creator, string projectId, string projectName, string contentType, uint32_t secondsToDeadline, uint64_t month) {
+  require_auth(creator);
+  uint32_t currentTime = current_time();
+  print("====== CURRENT TIME", currentTime);
+  uint32_t deadline = current_time() + secondsToDeadline;
+  print("====== DEADLINE ", deadline);
+
+  projects_table projects(_self, creator.value);
+  projects.emplace(_self, [&]( auto& row ) {
+    row.key = projects.available_primary_key();
+    row.isActive = true;
+    row.status = "In Progress";
+    row.projectId = projectId;
+    row.name = projectName;
+    row.contentType = contentType;
+    row.due = block_timestamp(deadline);
+    row.month = month;
+  });
+}
+
 /* FOR DEBUGGING PURPOSES */
-ACTION submanager::erasesub(name content_creator, name subber) {
+ACTION submerged::erasesub(name content_creator, name subber) {
   print("============ Removing ===============");
   channels_table subs(_self, content_creator.value);
   auto iterator = subs.find(subber.value);
@@ -77,18 +101,20 @@ ACTION submanager::erasesub(name content_creator, name subber) {
   subs.erase(iterator);
 }
 
+// ===================== ABI STUFF =========================
 
-extern "C" {
-  void apply(uint64_t receiver, uint64_t code, uint64_t action) {
-    submanager _submanager(name(receiver));
-    if(code==name("eosio.token").value && action==name("transfer").value) {
-      execute_action(name(receiver), name(code), &submanager::subscribe );
-    } else if(code==receiver && action==name("openchannel").value) {
-      execute_action(name(receiver), name(code), &submanager::openchannel );
-    } else if(code==receiver && action==name("rollfunds").value) {
-      execute_action(name(receiver), name(code), &submanager::rollfunds );
-    } else if (code==receiver && action==name("erasesub").value){
-      execute_action(name(receiver), name(code), &submanager::erasesub );
-    }
-  }
-};
+#define EOSIO_DISPATCH_CUSTOM( TYPE, MEMBERS ) \
+extern "C" { \
+   void apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
+      if( code == receiver ) { \
+         switch( action ) { \
+            EOSIO_DISPATCH_HELPER( TYPE, MEMBERS ) \
+         } \
+         /* does not allow destructor of thiscontract to run: eosio_exit(0); */ \
+      } else if (code==name("eosio.token").value && action==name("transfer").value) { \
+        execute_action(name(receiver), name(code), &submerged::transfer ); \
+      } \
+   } \
+} \
+
+EOSIO_DISPATCH_CUSTOM( submerged, (version)(open)(transfer) )
